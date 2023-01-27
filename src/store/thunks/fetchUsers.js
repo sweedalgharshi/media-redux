f6d9dac5363c4834a388438dd084d23b6e70db88
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-27 13:36:30 +0530

    steps for adding thunk

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,8 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get('http://localhost:3005');
+
+  return response.data;
+});
