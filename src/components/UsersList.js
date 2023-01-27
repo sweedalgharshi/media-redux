212a137f63aaf16c0b324c7aca288c1399d997bb
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-27 17:33:07 +0530

    wrapping the thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store';
+
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
   return <div>Userslist</div>;
 }
 
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchUsers = createAsyncThunk('users/fetch', async () => {
-  const response = await axios.get('http://localhost:3005');
+  const response = await axios.get('http://localhost:3005/users');
 
   return response.data;
 });
