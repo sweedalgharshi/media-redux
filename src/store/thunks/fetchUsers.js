4627f0f816bde10954f0eeef34da653620296a23
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-28 13:00:25 +0530

    added a skeleton loader and animation with tailwindCSS

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,5 +4,16 @@ import axios from 'axios';
 export const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
+  // DEV ONLY !!!!
+  await pause(2000);
+
   return response.data;
 });
+
+// DEV ONLY !!!!!!!
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
