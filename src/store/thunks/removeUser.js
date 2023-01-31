92ad42350ab36d694900beb28264318ce882cd75
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-31 12:50:13 +0530

    a thunk to delete user

---
+++ src/store/thunks/removeUser.js
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const removeUser = createAsyncThunk('users/remove', async (user) => {
+  const response = await axios.delete(`http://localhost:3005/users/${user.id}`);
+
+  // FIX!!
+  return response.data;
+});
