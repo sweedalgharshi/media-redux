92f663ca0ae7946fb6f6e095f7f828e4e7a52797
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-31 15:22:08 +0530

    fixing delete error

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -12,11 +12,13 @@ function UsersListItem({ user }) {
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
-        <Button loading={isRemoveUser} onClick={handleClick}>
-          <GoTrashcan />
-        </Button>
-        {removeUserError && <div>Error Deleting user...</div>}
-        {user.name}
+        <div className="flex flex-row justify-between items-center">
+          <Button className="mr-3" loading={isRemoveUser} onClick={handleClick}>
+            <GoTrashcan />
+          </Button>
+          {removeUserError && <div>Error Deleting user...</div>}
+          {user.name}
+        </div>
       </div>
     </div>
   );
