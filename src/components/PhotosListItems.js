102e2b7eb00e0f7be4a8bb46ed3b7bd44e1cbedb
sweedalgharshi <sweedalgracius@gmail.com>
2023-02-03 14:13:43 +0530

    adding automatic data refetching

--- src/components/PhotosListItems.js
+++ src/components/PhotosListItems.js
@@ -9,8 +9,8 @@ function PhotosListItems({ photo }) {
   };
 
   return (
-    <div onClick={handleRemovePhoto} className="relative m-2 cursor-pointer">
-      <img src={photo.url} alt="random pic" className="h-20 w-20" />
+    <div onClick={handleRemovePhoto} className="relative cursor-pointer m-2">
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
       <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
         <GoTrashcan className="text-3xl" />
       </div>
--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -10,6 +10,14 @@ const photosApi = createApi({
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        providesTags: (result, error, album) => {
+          const tags = result.map((photo) => {
+            return { type: 'Photo', id: photo.id };
+          });
+
+          tags.push({ type: 'AlbumPhoto', id: album.id });
+          return tags;
+        },
         query: (album) => {
           return {
             url: '/photos',
@@ -20,7 +28,11 @@ const photosApi = createApi({
           };
         },
       }),
+
       addPhoto: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: 'AlbumPhoto', id: album.id }];
+        },
         query: (album) => {
           return {
             method: 'POST',
@@ -32,7 +44,11 @@ const photosApi = createApi({
           };
         },
       }),
+
       removePhoto: builder.mutation({
+        invalidatesTags: (result, error, photo) => {
+          return [{ type: 'Photo', id: photo.id }];
+        },
         query: (photo) => {
           return {
             method: 'DELETE',
